ff017d70099ce14d142786d2df129ab1af07c131
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-23 09:11:05 +0700

    feat: update initial data

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,32 +1,56 @@
-/** @format */
-
-export const initialData = [
+const getInitialData = () => ([
   {
     id: 1,
     title: "Babel",
-    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama.",
+    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+    createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
-    createdAt: "2022-04-14T04:27:34.572Z",
   },
   {
     id: 2,
-    title: "React",
-    body: "React adalah library JavaScript untuk membangun antarmuka pengguna. React dikembangkan oleh Facebook dan digunakan untuk membangun aplikasi satu halaman (SPA) dan aplikasi mobile.",
+    title: "Functional Component",
+    body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
+    createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
-    createdAt: "2023-01-10T12:45:30.982Z",
   },
   {
     id: 3,
-    title: "Tailwind CSS",
-    body: "Tailwind CSS adalah framework CSS yang menawarkan pendekatan utilitas untuk styling. Dengan Tailwind, kita dapat membuat desain yang fleksibel dan cepat tanpa perlu menulis CSS yang berulang.",
-    archived: true,
-    createdAt: "2023-05-22T09:15:45.235Z",
+    title: "Modularization",
+    body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
+    createdAt: '2022-04-14T04:27:34.572Z',
+    archived: false,
   },
   {
     id: 4,
-    title: "Vite",
-    body: "Vite adalah build tool yang sangat cepat untuk pengembangan web modern. Vite dibuat oleh pencipta Vue.js dan menawarkan pengalaman pengembangan yang lebih baik dengan hot module replacement yang cepat.",
+    title: "Lifecycle",
+    body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
+    createdAt: '2022-04-14T04:27:34.572Z',
     archived: false,
-    createdAt: "2023-07-04T08:22:16.382Z",
   },
-];
+  {
+    id: 5,
+    title: "ESM",
+    body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
+    createdAt: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+  {
+    id: 6,
+    title: "Module Bundler",
+    body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
+    createdAt: '2022-04-14T04:27:34.572Z',
+    archived: false,
+  },
+]);
+
+const showFormattedDate = (date) => {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }
+  return new Date(date).toLocaleDateString("id-ID", options)
+}
+
+export { getInitialData, showFormattedDate };
